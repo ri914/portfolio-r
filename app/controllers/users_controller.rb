d04f49c7f0993ed4b708326b0a9b507f7cee9b1b
Apple <apple@Apples-MacBook-Air.local>
2025-02-01 08:44:35 +0900

    [update]設定ページを作成し、マイページを編集できるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if params[:user][:remove_avatar] == "1"
+      @user.avatar.purge
+    end
+
+    if @user.update(params.require(:user).permit(:name, :avatar))
+      redirect_to user_path(@user), notice: t('devise.registrations.account_updated')
+    else
+      render 'devise/registrations/edit'
+    end
+  end
+
   private
 
   def user_params

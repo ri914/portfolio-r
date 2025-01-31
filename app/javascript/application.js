b3c8eed1ac7cfd1f8c677028e00bdf529a8c3a67
Apple <apple@Apples-MacBook-Air.local>
2025-02-01 08:44:35 +0900

    [update]設定ページを作成し、マイページを編集できるように

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -22,3 +22,16 @@ $('.login').on('submit', function(e) {
     }, 4000);
   }, 3000);
 });
+
+$(document).ready(function() {
+  $('.file-input').on('change', function() {
+    var input = this;
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('.current-avatar-container img').attr('src', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  });
+});

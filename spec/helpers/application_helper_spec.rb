28c6cb0f421f1f52a6f9255ee903e7aabb6abe94
Apple <apple@Apples-MacBook-Air.local>
2025-02-02 14:36:53 +0900

    [update]helperのrspecを作成

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#full_title' do
+    let(:base_title) { "温泉マッチング" }
+
+    it 'ページタイトルが提供されている時、フルタイトルを返すこと' do
+      expect(helper.full_title(page_title: 'Page Title')).to eq("#{base_title} - Page Title")
+    end
+
+    it 'ページタイトルが空の時、基本タイトルだけを返すこと' do
+      expect(helper.full_title(page_title: '')).to eq(base_title)
+    end
+
+    it 'ページタイトルがnilの時、基本タイトルだけを返すこと' do
+      expect(helper.full_title(page_title: nil)).to eq(base_title)
+    end
+  end
+end

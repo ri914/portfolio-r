77d6bbfd46172e8f129b5df27b248ec7c61ec08d
Apple <apple@Apples-MacBook-Air.local>
2025-02-02 14:52:44 +0900

    [update]rubocop導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
     email == 'guest@example.com'
   end
 
-
   has_many :onsens, dependent: :destroy
   has_many :saved_onsens, class_name: 'Onsen', foreign_key: 'saved_by_user_id'
   has_one_attached :avatar

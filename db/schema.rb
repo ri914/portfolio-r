ef76a6a1031f393574d770e9db97430d9c0d4704
Apple <apple@Apples-MacBook-Air.local>
2025-02-15 18:32:02 +0900

    [update]同じ名前の温泉地を投稿できないように

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_14_052319) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_15_091359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_14_052319) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "image_descriptions", force: :cascade do |t|
+    t.bigint "onsen_id", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["onsen_id"], name: "index_image_descriptions_on_onsen_id"
+  end
+
   create_table "onsens", force: :cascade do |t|
     t.string "name", null: false
     t.string "location"
@@ -55,8 +63,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_14_052319) do
     t.integer "saved_by_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "image_descriptions"
-    t.string "image_description"
     t.index ["saved_by_user_id"], name: "index_onsens_on_saved_by_user_id"
     t.index ["user_id"], name: "index_onsens_on_user_id"
   end
@@ -90,5 +96,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_14_052319) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "image_descriptions", "onsens"
   add_foreign_key "onsens", "users"
 end

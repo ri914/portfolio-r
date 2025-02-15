02b8e9b5a5d6acfe83a1f1b471e40c0ec39c0bbf
Apple <apple@Apples-MacBook-Air.local>
2025-02-15 15:49:38 +0900

    [update]ゲストユーザーが直接urlを入力して設定、投稿フォームへ遷移できないように

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest_user, only: [:edit]
+
   def new
     @page_title = "新規登録"
     super
@@ -18,5 +20,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     home_index_path
+  end
+
+  private
+
+  def check_guest_user
+    if current_user.guest?
+      flash[:alert] = 'ゲストユーザーはこの機能を使用できません。'
+      redirect_back(fallback_location: home_index_path)
+    end
   end
 end

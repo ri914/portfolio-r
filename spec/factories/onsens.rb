dc55a396f8bb0b30d889196d42b84c9ab1ef42bd
Apple <apple@Apples-MacBook-Air.local>
2025-03-01 17:17:07 +0900

    [update]温泉の一覧ページのrspecを追加

--- spec/factories/onsens.rb
+++ spec/factories/onsens.rb
@@ -1,21 +1,23 @@
-# spec/factories/onsens.rb
 FactoryBot.define do
   factory :onsen do
-    name { "箱根温泉" }
-    location { "神奈川県" }
-    description { "素晴らしい温泉です。" }
-
-    # 既存の泉質を自動的に作成する
-    after(:create) do |onsen|
-      create_list(:water_quality, 12) unless WaterQuality.count > 0 # 泉質が既に存在しない場合のみ作成
-      onsen.water_qualities << WaterQuality.all # すべての泉質を関連付ける
-    end
+    association :user
+    name { '箱根温泉' }
+    location { '神奈川県' }
+    region { '関東' }
+    water_quality_ids { [] }
 
     after(:build) do |onsen|
-      if onsen.images.blank?
-        onsen.images.attach(io: File.open(Rails.root.join('spec/fixtures/files/sample_image.jpg')), filename: 'sample_image.jpg',
-                            content_type: 'image/jpeg')
-      end
+      onsen.images.attach(
+        io: File.open(Rails.root.join('spec/fixtures/files/sample_image.jpg')),
+        filename: 'sample_image.jpg',
+        content_type: 'image/jpeg'
+      )
+    end
+
+    trait :in_aomori do
+      name { '酸ヶ湯温泉' }
+      location { '青森県' }
+      region { '東北' }
     end
   end
 end

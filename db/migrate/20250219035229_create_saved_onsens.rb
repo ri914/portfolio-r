cd76fdc14d812bce8c8a6b2cff9dcf08e33bfb7e
Apple <apple@Apples-MacBook-Air.local>
2025-03-02 16:01:47 +0900

    [fix]マイグレーション修正

--- db/migrate/20250219035229_create_saved_onsens.rb
+++ db/migrate/20250219035229_create_saved_onsens.rb
@@ -1,10 +1,8 @@
 class CreateSavedOnsens < ActiveRecord::Migration[7.1]
   def change
-    create_table :onsens do |t|
-      t.string :name
-      t.string :location
-      t.text :description
+    create_table :saved_onsens do |t|
       t.references :user, null: false, foreign_key: true
+      t.references :onsen, null: false, foreign_key: true
 
       t.timestamps
     end

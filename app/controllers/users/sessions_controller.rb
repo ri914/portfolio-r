2f7f3576ac597d362832d1c75434179016ea1c8a
Apple <apple@Apples-MacBook-Air.local>
2025-03-04 17:50:06 +0900

    [update]トップページで検索したとき、自動でゲストログインされるように

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,17 @@ class Users::SessionsController < Devise::SessionsController
       user.password = SecureRandom.urlsafe_base64
       user.name = 'ゲストユーザー'
     end
-    sign_in guest_user
 
-    redirect_to home_index_path, notice: t('devise.sessions.guest_logged_in')
+    sign_in guest_user
+    keyword = params[:keyword]
+
+    if keyword.present?
+      redirect_to search_onsens_path(keyword: keyword), notice: t('devise.sessions.guest_logged_in')
+    else
+      redirect_to home_index_path, notice: t('devise.sessions.guest_logged_in')
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    redirect_to root_path, alert: "ゲストログインに失敗しました: #{e.message}"
   end
 
   def after_sign_in_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
+  
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
   devise_scope :user do

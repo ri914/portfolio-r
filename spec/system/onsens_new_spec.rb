347886a185bc8c8f729359cd5db2c25f1dea7c82
Apple <apple@Apples-MacBook-Air.local>
2025-03-11 16:02:44 +0900

    [fix]システムスペック修正途中

--- spec/system/onsens_new_spec.rb
+++ spec/system/onsens_new_spec.rb
@@ -12,14 +12,14 @@ RSpec.describe '新規投稿ページ', type: :system do
     it '温泉を投稿できる' do
       visit new_onsen_path
 
-      fill_in 'onsen[name]', with: '箱根温泉'
+      fill_in 'onsen[name]', with: '箱根温泉郷'
       select '神奈川県', from: 'onsen[location]'
       check '単純温泉', visible: :all
       attach_file "onsen[images][]", Rails.root.join('spec/fixtures/files/sample_image.jpg')
       click_button '投稿'
 
       expect(page).to have_content '温泉情報が作成されました。'
-      expect(page).to have_content '箱根温泉'
+      expect(page).to have_content '箱根温泉郷'
     end
   end
 end
